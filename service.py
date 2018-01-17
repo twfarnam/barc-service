68321480aeb8baf2db3eac10f42deac4c6aaa87a
Tim Farnam <twfarnam@gmail.com>
2018-01-17 16:39:13 -0600

    turns of debugging by default

--- service.py
+++ service.py
@@ -268,6 +268,7 @@ def ai(image):
 # SETUP
 
 if __name__ == '__main__':
-    # load_nn()
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    load_nn()
+    app.run(port=5000, host='0.0.0.0')
+    # app.run(debug=True, port=5000, host='0.0.0.0')
 
